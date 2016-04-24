a0e4b0411f0efafe9d6dbc202afccc65069c033d
Irmak Sirer <irmak.sirer@datascopeanalytics.com>
2016-04-24 00:30:41 -0500

    video player page done

--- app.py
+++ app.py
@@ -3,9 +3,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
+def choochoo():
     return render_template('choochoo.html')
+
+
+@app.route('/video/<vidname>')
+def video_player(vidname):
+    return render_template('video_player.html',
+                           vidname=vidname)
 
 
 if __name__ == '__main__':
